9e7ab69f7c5f7d62a796db4979560d8d024180f3
ArtemKhudiakov <artemka86ber@gmail.com>
2022-06-27 00:22:34 -0300

    create first les

--- first.ts
+++ first.ts
@@ -16,3 +16,16 @@ o = false
 // nothing
 let h: undefined = undefined;
 let g: null = null
+
+// literal
+const num = 108
+const str2 = 'str'
+
+// universal
+let mm: any = 1;
+mm = 'str'
+mm = []
+mm.toUpperCase()
+
+let xx: unknown = 2
+if (typeof xx === "string") xx.toUpperCase()
