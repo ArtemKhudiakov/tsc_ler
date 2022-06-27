5376571ac1f10cb7fdd8c1126516b1e40bb98322
ArtemKhudiakov <artemka86ber@gmail.com>
2022-06-27 00:21:01 -0300

    create first les

---
+++ first.ts
@@ -0,0 +1,18 @@
+// numbers
+let x = 10.5;
+// let y = 12345n;
+let z = NaN;
+
+// string
+let str1: string
+str1 = 'Hello world'
+// let d = str1 + symb
+
+// boolean
+let o = true
+o = false
+// o = 1
+
+// nothing
+let h: undefined = undefined;
+let g: null = null
