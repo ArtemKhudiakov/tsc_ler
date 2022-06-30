0e19734695c0f6dfbcd66db7a27f1af95a72c92b
ArtemKhudiakov <artemka86ber@gmail.com>
2022-06-30 14:18:29 -0300

    update third les

--- base/third.ts
+++ base/third.ts
@@ -7,3 +7,17 @@ interface Car {
 
     [key: string]: unknown;
 }
+
+const car: Car = {
+    wheels: 4,
+    brand: 'BMW',
+    type: 'Sedan',
+}
+
+const car2: Car = {
+    brand: 'BMW',
+    type: '',
+    wheels: 4,
+}
+
+car2['go'] = true;
