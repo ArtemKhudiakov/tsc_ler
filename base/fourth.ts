8ae654ae97888c1a459156d9a5b7c6867d96e4da
ArtemKhudiakov <artemka86ber@gmail.com>
2022-07-01 12:32:36 -0300

    update fourth task

--- base/fourth.ts
+++ base/fourth.ts
@@ -28,3 +28,49 @@ function printName(user: {
 
 printName({firstName: 'Mikhail'})
 printName({firstName: 'Mikhail', lastName: 'Nep'})
+
+//task
+interface User {
+    login: string;
+    email: string;
+    password: string;
+    isOnline: boolean;
+    lastVisited: Date;
+}
+
+interface Admin {
+    login: string;
+    email: string;
+    password: string;
+    isOnline: boolean;
+    lastVisited: Date;
+    role: string;
+}
+
+const user1: User = {
+    login: 'michey',
+    email: 'some@email.com',
+    isOnline: false,
+    password: '12345',
+    lastVisited: new Date(2022, 6, 2),
+}
+
+const admin1: Admin = {
+    login: 'michey',
+    email: 'some@email.com',
+    isOnline: false,
+    password: '12345',
+    lastVisited: new Date(2022, 6, 2),
+    role: 'SuperAdmin',
+}
+
+// Create a function login with param user (with keys login and password)
+// Check if login and password are not empty and then greet user
+function login(user: { login: string, password: string }) {
+    if (user.login.length > 0, user.password.length > 0) {
+        console.log('Hello', user.login)
+    }
+}
+
+login(user1)
+login(admin1)
