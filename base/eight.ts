275428f727cd9fd5085577d067f77d0e9de190e1
ArtemKhudiakov <artemka86ber@gmail.com>
2022-07-04 01:11:57 -0300

    feat funcprintid

--- base/eight.ts
+++ base/eight.ts
@@ -3,3 +3,10 @@ type Status = 'ok' | 'loading' | 'error';
 const staticX: Status = 'loading';
 
 const arr: (number | string)[] = []
+
+function printId(id: number | string): void {
+    if (typeof id === 'string')
+        console.log(id.toUpperCase())
+    else
+        console.log(id)
+}
