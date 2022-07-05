9361cb295731177913d16a6f45df2c1a72d6da9c
ArtemKhudiakov <artemka86ber@gmail.com>
2022-07-05 10:26:28 -0300

    update enums

--- base/ninth.ts
+++ base/ninth.ts
@@ -4,3 +4,18 @@ enum ShapeKind {
 }
 
 const myShape = ShapeKind.Circle;
+
+interface Circle {
+    kind: ShapeKind.Circle;
+    radius: number;
+}
+
+interface Square {
+    kind: ShapeKind.Square;
+    sideLength: number;
+}
+
+const circle1: Circle = {
+    radius: 2,
+    kind: ShapeKind.Circle,
+}
