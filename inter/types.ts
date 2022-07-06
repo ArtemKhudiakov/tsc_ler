3f44d33c26de9ae1b9b7f1a0d5ecd007e5959225
ArtemKhudiakov <artemka86ber@gmail.com>
2022-07-06 17:59:20 -0300

    done type tasks

--- inter/types.ts
+++ inter/types.ts
@@ -48,4 +48,22 @@ interface InternetOrder extends Order {
     email: string;
 }
 
-type PossibleOrders = TelephoneOrder | InternetOrder | undefined;
+type PossibleOrders = TelephoneOrder | InternetOrder | undefined
+
+function isAnInternetOrder(order: PossibleOrders): order is InternetOrder {
+    return !!order && "email" in order;
+}
+
+function isATelephoneOrder(order: PossibleOrders): order is TelephoneOrder {
+    return !!order && "callerNumber" in order;
+}
+
+function makeOrder(order: PossibleOrders) {
+    if (isAnInternetOrder(order)) {
+        console.log(order.email);
+    } else if (isATelephoneOrder(order)) {
+        console.log(order.callerNumber);
+    }
+}
+
+export {}
