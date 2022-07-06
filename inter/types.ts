ff5150e61fc04c03fe352d89f84062170e58d1aa
ArtemKhudiakov <artemka86ber@gmail.com>
2022-07-06 17:51:02 -0300

    update types widen

--- inter/types.ts
+++ inter/types.ts
@@ -4,3 +4,15 @@ type Union3 = Union1 | Union2; // all variants
 type Union4 = Union1 & Union2; // a | c
 
 type Union5 = { a: string, b: string, c: number } & { a: string, t: boolean, z: null }
+
+type User = {
+    readonly email: string,
+    readonly login: string,
+    password: string,
+}
+type Person = {
+    readonly firstName: string,
+    lastName: string,
+    phone?: string,
+    yearOfBirth?: number,
+}
