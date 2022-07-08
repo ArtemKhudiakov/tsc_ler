b017a415145862d8318b29ba68ef5dfec4f6eb76
ArtemKhudiakov <artemka86ber@gmail.com>
2022-07-07 22:34:14 -0300

    create merge

--- inter/types.ts
+++ inter/types.ts
@@ -66,4 +66,46 @@ function makeOrder(order: PossibleOrders) {
     }
 }
 
+interface IProduct {
+    title: string,
+    price: number,
+    isNew: boolean,
+    isSale?: boolean,
+}
+
+interface IVehicle {
+    wheels: number,
+    year: number,
+    brand: string,
+}
+
+interface ICar extends IProduct, IVehicle {
+    type: string,
+    model: string,
+}
+
+// const icar1: ICar = {}
+
+type Product = {
+    title: string,
+    price: number,
+    isNew: boolean,
+    isSale?: boolean,
+}
+
+type Vehicle = {
+    wheels: number,
+    year: number,
+    brand: string,
+}
+
+type Car = Product & Vehicle & {
+    type: string,
+    model: string,
+}
+
+
 export {}
+
+
+
